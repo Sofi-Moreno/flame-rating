b287f887f60b1659ee67e702a9e69cc189faea09
Manuel Antias <meantias.23@est.ucab.edu.ve>
2025-11-08 22:51:30 -0400

    Reparación error de sintaxys V.1.2.1

--- back/back-flame-rating/src/main/java/com/flamerating/back_flame_rating/repository/IReviewRepository.java
+++ back/back-flame-rating/src/main/java/com/flamerating/back_flame_rating/repository/IReviewRepository.java
@@ -1,5 +1,11 @@
 package com.flamerating.back_flame_rating.repository;
 
-public interface IReviewRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import com.flamerating.back_flame_rating.model.Review;
+
+@Repository
+public interface IReviewRepository extends JpaRepository <Review, Integer> {
+    
 }
--- back/back-flame-rating/src/main/java/com/flamerating/back_flame_rating/service/ReviewService.java
+++ back/back-flame-rating/src/main/java/com/flamerating/back_flame_rating/service/ReviewService.java
@@ -1,5 +1,49 @@
 package com.flamerating.back_flame_rating.service;
 
-public class ReviewService {
+import java.util.List;
+import org.springframework.stereotype.Service;
+import com.flamerating.back_flame_rating.model.Review;
+import com.flamerating.back_flame_rating.repository.IReviewRepository;
 
+@Service
+public class ReviewService implements IReviewService {
+
+    private final IReviewRepository iReviewRepository;
+
+    
+
+    public ReviewService(IReviewRepository iReviewRepository) {
+        this.iReviewRepository = iReviewRepository;
+    }
+
+    @Override
+    public Review saveReview(Review review) {
+        return iReviewRepository.save(review);
+    }
+
+    @Override
+    public List<Review> findAll() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Review findById(Integer id) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void deleteReview(Integer id) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public Review updateReview(Review review) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    
 }
