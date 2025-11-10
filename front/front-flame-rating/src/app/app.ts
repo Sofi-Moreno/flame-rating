2cc41c3a0312490f2f2c65ceb80567da8d9be917
Sofia Moreno <sgmoreno.23@est.ucab.edu.ve>
2025-11-09 22:51:20 -0400

    Se arreglo problema de merge V.1.9.1

--- front/front-flame-rating/src/app/app.ts
+++ front/front-flame-rating/src/app/app.ts
@@ -1,18 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-<<<<<<< HEAD
-import { MainMenu } from "./components/main-menu/main-menu";
-
-@Component({
-  selector: 'app-root',
-  imports: [RouterOutlet, MainMenu],
-=======
+import { MainMenu } from "./main-menu/main-menu";
 import { Encabezado } from "./encabezado/encabezado";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Encabezado],
->>>>>>> 7d7294f9b2edc80c3725047861581a8b560e95a3
+  imports: [RouterOutlet, Encabezado,MainMenu],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
