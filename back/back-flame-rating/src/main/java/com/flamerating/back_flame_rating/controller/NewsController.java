99f18948627fdc883f204f3390b954237e297560
Sofia Moreno <sgmoreno.23@est.ucab.edu.ve>
2025-11-10 13:38:37 -0400

    Se agrego la etiqueta de http://localhost:4200 en los controladores V.2.1.0

--- back/back-flame-rating/src/main/java/com/flamerating/back_flame_rating/controller/NewsController.java
+++ back/back-flame-rating/src/main/java/com/flamerating/back_flame_rating/controller/NewsController.java
@@ -2,6 +2,8 @@ package com.flamerating.back_flame_rating.controller;
 
 import com.flamerating.back_flame_rating.model.News;
 import com.flamerating.back_flame_rating.service.NewsService;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,6 +16,7 @@ import java.util.List;
 comunicarse con los clientes mediante Json.*/
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class NewsController {
     private final NewsService newsService;
 
