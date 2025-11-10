bc3ab97a891feacdd3d7a533480367898be33061
Manuel Antias <meantias.23@est.ucab.edu.ve>
2025-11-09 21:49:57 -0400

    Se mejoró el manejo de excepciones en el controlador de reseñas V.1.6.6.

--- back/back-flame-rating/src/main/java/com/flamerating/back_flame_rating/controller/ReviewController.java
+++ back/back-flame-rating/src/main/java/com/flamerating/back_flame_rating/controller/ReviewController.java
@@ -1,7 +1,9 @@
 package com.flamerating.back_flame_rating.controller;
 
 import java.util.List;
-
+import java.util.NoSuchElementException;
+import org.springframework.http.HttpStatus; 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,7 +11,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.flamerating.back_flame_rating.service.ReviewService;
 import com.flamerating.back_flame_rating.model.Review;
 
@@ -32,38 +33,86 @@ cliente en un objeto de tipo Review(Java) si tener que usar alguna librería.*/
 
 /*http://localhost:8080/create-review*/
 @PostMapping ("/create-review")
-    public Review save(@RequestBody Review review) {
-        return reviewService.saveReview(review); //Guarda un objeto del tipo service
+    public ResponseEntity<?> saveReview(@RequestBody Review review) {
+        try{
+            Review savedReview = reviewService.saveReview(review);
+            return ResponseEntity.status(HttpStatus.CREATED).body(savedReview);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error saving review: " + e.getMessage());
+        }
     }
 
 @GetMapping ("/reviews")
-    public List<Review> findAll() {
-        return reviewService.findAll();
+    public ResponseEntity<?> findAll() {
+        try{
+            List<Review> reviews = reviewService.findAll();
+            return ResponseEntity.ok(reviews);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error searching for all reviews: " + e.getMessage());
+        }
     }
 
 /*http://localhost:8080/by-game/{id}*/
 @GetMapping("/reviews/by-game/{id}")
-    public List<Review> getReviewsByGameId(@PathVariable Integer id) {
-        return reviewService.findByVideoGameId(id);
+    public ResponseEntity<?> getReviewsByGameId(@PathVariable Integer id) {
+        try{
+            List<Review> reviews = reviewService.findByVideoGameId(id);
+            return ResponseEntity.ok(reviews);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error searching for reviews by Id: " + id + " of videogame: " + e.getMessage());
+        }
     }
 
 /*http://localhost:8080/delete-review/{id}*/
 @DeleteMapping("/delete-review/{id}")
-    public void deleteReview(@PathVariable Integer id) {
-        reviewService.deleteReview(id);
+    public ResponseEntity<?> deleteReview(@PathVariable Integer id) {
+        try{
+            reviewService.deleteReview(id);
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).body("review deleted successfully.");
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error deleting review with id: " + id + " : " + e.getMessage());
+        }
     }
 
+/*http://localhost:8080/review/{id}*/
 @GetMapping ("/review/{id}")
-    public Review findById(@PathVariable Integer id) {
-        return reviewService.findById(id);
+    public ResponseEntity<?> findById(@PathVariable Integer id) {
+        try{
+            Review review = reviewService.findById(id);
+            return ResponseEntity.ok(review);
+        }
+        catch (NoSuchElementException e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Error searching review by Id: " + id + " : " + e.getMessage());
+        }
     }
 
+/*http://localhost:8080/update-review*/
 @PutMapping ("/update-review")
-    public Review updateReview(@RequestBody Review review) {
-        Review reviewDb = reviewService.findById(review.getId());
-        reviewDb.setIdUser(review.getIdUser());
-        reviewDb.setRating(review.getRating());
-        reviewDb.setComment(review.getComment());
-        return reviewService.updateReview(reviewDb);
+    public ResponseEntity<?> updateReview(@RequestBody Review review) {
+        try {
+                Review reviewDb = reviewService.findById(review.getId());
+                reviewDb.setIdUser(review.getIdUser());
+                reviewDb.setRating(review.getRating());
+                reviewDb.setComment(review.getComment());
+                Review updatedReview = reviewService.saveReview(reviewDb);
+                return ResponseEntity.ok(updatedReview);
+        }
+        catch(NoSuchElementException e){
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Error updating review with id: " + review.getId() + " : " + e.getMessage()); //Id don't exist
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error updating review with id: " + review.getId() + " : " + e.getMessage()); //Incorrect data entered
+        }
     }
 }
---
+++ back/back-flame-rating/src/main/java/com/flamerating/serverExcepcion/ExcecptionHandler.java
@@ -0,0 +1,7 @@
+package com.flamerating.serverException;
+
+public @interface ExcecptionHandler {
+
+    Class<ResourceNotFoundException> value();
+
+}
