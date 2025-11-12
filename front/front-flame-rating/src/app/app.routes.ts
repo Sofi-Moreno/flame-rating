d4b717fb6d1689bd74e4c5c29e852f56b1b02cf5
aedelarosa <aedelarosa.24@est.ucab.edu.ve>
2025-11-12 11:58:31 -0400

    create-news y view-news casi listo V.2.15.0.

--- front/front-flame-rating/src/app/app.routes.ts
+++ front/front-flame-rating/src/app/app.routes.ts
@@ -1,16 +1,21 @@
+
+
 import { Routes } from '@angular/router';
 import { LoginRegister } from './login-register/login-register';
 import { ViewProfile } from './view-profile/view-profile';
 import { MainMenu } from './main-menu/main-menu';
 import { ViewNews } from './view-news/view-news';
-import { CreateNews } from './create-news/create-news';
-import { ViewVideogame } from './view-videogame/view-videogame';
+
+
+import { CreateNewsComponent } from './create-news/create-news'; 
 
 export const routes: Routes = [
     { path: 'login-register', component: LoginRegister },
     { path: 'view-profile', component: ViewProfile},
-    {path:"", component: MainMenu},
+    { path:"", component: MainMenu},
     { path: 'view-news', component: ViewNews},
-    { path: 'create-news', component: CreateNews},
-    { path: 'videogame/:id', component: ViewVideogame}
+    
+  
+    { path: 'create-news', component: CreateNewsComponent }
+
 ];
